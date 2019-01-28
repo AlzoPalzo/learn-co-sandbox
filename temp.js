9f28f76ad894ebd4677fe7572e466da84f2f4537
Ali Madni <ali555m@hotmail.com>
2019-01-28 14:14:18 +0000

    Automatically backed up by Learn

--- temp.js
+++ temp.js
@@ -1,12 +1,4 @@
-// declare the variable
-var word
- 
-// assign a value to the variable
-word = 'bird'
- 
-console.log(word) // 'bird'
- 
-// assign another value to the variable
-word = 'dog'
- 
-console.log(word) // 'dog'
+function sayHello()
+{
+  console.log('Hello!')
+}
